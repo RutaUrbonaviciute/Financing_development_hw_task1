d6dea90a0824279bf676c96cd5b7510811f840d2
RutaUrbonaviciute <ruta.urbonaviciute@hotmail.com>
2020-07-03 02:03:28 +0300

    final commit

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ fetch(fileUrl)
   .then(r => r.text())
   .then(t => alert(polymerReduction(t.split(''))))
 
-
 const polymerReduction = (arr) => {
   for (let i = 0; i < arr.length - 1; i++) {
     if (arr[i].toLowerCase() == arr[i + 1].toLowerCase() && arr[i] !== arr[i + 1]) {
@@ -15,7 +14,6 @@ const polymerReduction = (arr) => {
         arr.splice(i - 1, 2)
       }
     }
-
   }
   return arr.length
 }
