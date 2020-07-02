412b58672347d97aa6d8b21a53097d72717f767c
RutaUrbonaviciute <ruta.urbonaviciute@hotmail.com>
2020-07-03 01:57:03 +0300

    add task logic

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Financial development task 1
+## Polymer reduction
+### answer -> 21859
---
+++ app.js
@@ -0,0 +1,21 @@
+const fileUrl = './input.txt'
+
+fetch(fileUrl)
+  .then(r => r.text())
+  .then(t => alert(polymerReduction(t.split(''))))
+
+
+const polymerReduction = (arr) => {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i].toLowerCase() == arr[i + 1].toLowerCase() && arr[i] !== arr[i + 1]) {
+      arr.splice(i, 2)
+    }
+    if (i > 1) {
+      if (arr[i].toLowerCase() == arr[i - 1].toLowerCase() && arr[i] !== arr[i - 1]) {
+        arr.splice(i - 1, 2)
+      }
+    }
+
+  }
+  return arr.length
+}
